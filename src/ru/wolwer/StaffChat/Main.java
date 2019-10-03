aafe00ec5af7c6e21f44c0b495da28cc5807fbfc
W0lweR <42712257+W0lweR@users.noreply.github.com>
2019-10-03 16:14:24 +0300

    Add files via upload

---
+++ src/ru/wolwer/StaffChat/Main.java
@@ -0,0 +1,48 @@
+package ru.wolwer.StaffChat;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+  public void onEnable() {
+    getLogger().info("StaffChat Enabled!");
+    getConfig().options().copyDefaults();
+    saveDefaultConfig();
+  }
+
+  public void onDisable() {
+    getLogger().info("StaffChat Disabled!");
+  }
+
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    if (sender instanceof Player) {
+      Player player = (Player) sender;
+      if (player.hasPermission(getConfig().getString("staffchat-send-permission"))) {
+        if (args.length > 0) {
+          String message = "";
+          for (int i = 0; i < args.length; i++) {
+            String arg = args[i] + " ";
+            message = message + arg;
+          }
+          for (Player p : Bukkit.getOnlinePlayers()) {
+            if (p.hasPermission(getConfig().getString("staffchat-view-permission"))) {
+              p.sendMessage(ChatColor.translateAlternateColorCodes('&', (ChatColor.translateAlternateColorCodes('&', getConfig().getString("staffchat-prefix")  + player.getDisplayName() + getConfig().getString("staffchat-message-format") + message))));
+            }
+          }
+        }else {
+          player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("staffchat-no-arguments")));
+        }
+      }else {
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("staffchat-no-permission-message")));
+      }
+    } else {
+      System.out.println("You must be a player to execute this command!");
+    }
+    return true;
+  }
+}
